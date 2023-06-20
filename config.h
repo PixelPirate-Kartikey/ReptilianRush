56a231fbfde37f6289a996a225728170ddb02d23
Kartikey Saxena <104156929+Kartikey3107@users.noreply.github.com>
2023-06-20 20:51:40 +0530

    Add files via upload

---
+++ config.h
@@ -0,0 +1,19 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+// Game Settings
+const int Width = 20;
+const int Height = 20;
+const int InitialDelay = 200;
+const int ScoreIncrement = 10;
+const int MaxTailLength = 100;
+
+// Key Inputs
+const int KeyLeft = 75;
+const int KeyRight = 77;
+const int KeyUp = 72;
+const int KeyDown = 80;
+const int KeyPause = 'p';
+const int KeyQuit = 'x';
+
+#endif
